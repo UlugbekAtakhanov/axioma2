b9a2e9bb9cd3a7c5aa4281c9e05cb28e5fcaaf58
Ulugbek Atakhanov <atakhanov_ux@mail.ru>
2023-07-01 16:39:25 +0500

    July1 16:39

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState, useEffect } from "react"
 import { createRoutesFromElements, Route, Outlet } from 'react-router-dom'
 import HomePageLayout from './layouts/HomePageLayout'
 import HomePage from './pages/HomePage'
 
+import { motion } from "framer-motion"
+
+import logo from "./assets/logo.png"
+
 const App = () => {
+	const [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false)
+		}, 3000);
+	}, [])
+
 	return (
 		<div>
+			<motion.div animate={{ opacity: isLoading ? 1 : 0, pointerEvents: "none" }} className="fixed inset-0 z-10 bg-black flex justify-center items-center overflow-hidden">
+				<motion.div
+					initial={{ rotate: 0 }}
+					animate={{ rotate: 360 }}
+					transition={{ duration: 2, ease: "linear", repeat: Infinity }}
+					className=" border-8 border-t-customYellow border-r-customYellow w-[200px] h-[200px] p-3 rounded-full flex justify-center items-center">
+				</motion.div>
+				<img className="absolute " src={logo} alt="img" />
+			</motion.div>
+
 			<Outlet />
-			{/* <div className="h-screen"></div> */}
+
 		</div>
 	)
 }
